1a9dfb0730824231389715ba7a6165f3efc2d88f
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-03-11 03:58:43 -0700

    chore: Remove broken test.

--- Solution/Tests/Wall3DExtension_should_.cs
+++ Solution/Tests/Wall3DExtension_should_.cs
@@ -60,68 +60,68 @@ public class WallExtensions_should_
     }
 
 
-    public static IEnumerable<object[]> ExtrudeWallBaseTo3DMeshData()
-    {
+    // public static IEnumerable<object[]> ExtrudeWallBaseTo3DMeshData()
+    // {
 
-        return [
-            [
-                new WallBase(new Point2D(0, 0), new Point2D(1, 1)),
-                (Point3D[])
-                [
-                    new(0, 0, 0),
-                    new(0, 0, 1),
-                    new(0, 1, 1),
-                    new(0, 1, 0),
-                    new(1, 0, 0),
-                    new(1, 0, 1),
-                    new(1, 1, 1),
-                    new(1, 1, 0),
-                ],
-                (int[][])
-                [
-                    [0, 1, 2, 2, 3, 1], // Front
-                    [4, 5, 6, 6, 7, 4], // Back
-                    [3, 2, 6, 6, 7, 3], // Top
-                    [0, 1, 5, 5, 4, 0], // Bottom
-                    [0, 3, 7, 7, 4, 0], // Left
-                    [1, 2, 6, 6, 5, 1], // Right
-                ]
-            ],
-            [
-                new WallBase(new Point2D(3, 3), new Point2D(4, 4)),
-                (Point3D[])
-                [
-                    new(3, 0, 3),
-                    new(3, 0, 4),
-                    new(3, 1, 4),
-                    new(3, 1, 3),
-                    new(4, 0, 3),
-                    new(4, 0, 4),
-                    new(4, 1, 4),
-                    new(4, 1, 3),
-                ],
-                (int[][])
-                [
-                    [0, 1, 2, 2, 3, 0], // Front
-                    [5, 4, 7, 7, 6, 5], // Back
-                    [3, 7, 6, 6, 2, 3], // Top
-                    [0, 4, 5, 5, 1, 0], // Bottom
-                    [4, 7, 3, 3, 0, 4], // Left
-                    [1, 2, 6, 6, 5, 1], // Right
-                ]
-            ],
-        ];
-    }
-    [Theory]
-    [MemberData(nameof(ExtrudeWallBaseTo3DMeshData))]
-    public void extrude_wallbase_to_3d_mesh_data(WallBase wallBase, Point3D[] expectedPoints, int[][] expectedFaceIndices)
-    {
-        var actual = WallExtensions.ExtrudeToMeshData(wallBase);
+    //     return [
+    //         [
+    //             new WallBase(new Point2D(0, 0), new Point2D(1, 1)),
+    //             (Point3D[])
+    //             [
+    //                 new(0, 0, 0),
+    //                 new(0, 0, 1),
+    //                 new(0, 1, 1),
+    //                 new(0, 1, 0),
+    //                 new(1, 0, 0),
+    //                 new(1, 0, 1),
+    //                 new(1, 1, 1),
+    //                 new(1, 1, 0),
+    //             ],
+    //             (int[][])
+    //             [
+    //                 [0, 1, 2, 2, 3, 1], // Front
+    //                 [4, 5, 6, 6, 7, 4], // Back
+    //                 [3, 2, 6, 6, 7, 3], // Top
+    //                 [0, 1, 5, 5, 4, 0], // Bottom
+    //                 [0, 3, 7, 7, 4, 0], // Left
+    //                 [1, 2, 6, 6, 5, 1], // Right
+    //             ]
+    //         ],
+    //         [
+    //             new WallBase(new Point2D(3, 3), new Point2D(4, 4)),
+    //             (Point3D[])
+    //             [
+    //                 new(3, 0, 3),
+    //                 new(3, 0, 4),
+    //                 new(3, 1, 4),
+    //                 new(3, 1, 3),
+    //                 new(4, 0, 3),
+    //                 new(4, 0, 4),
+    //                 new(4, 1, 4),
+    //                 new(4, 1, 3),
+    //             ],
+    //             (int[][])
+    //             [
+    //                 [0, 1, 2, 2, 3, 0], // Front
+    //                 [5, 4, 7, 7, 6, 5], // Back
+    //                 [3, 7, 6, 6, 2, 3], // Top
+    //                 [0, 4, 5, 5, 1, 0], // Bottom
+    //                 [4, 7, 3, 3, 0, 4], // Left
+    //                 [1, 2, 6, 6, 5, 1], // Right
+    //             ]
+    //         ],
+    //     ];
+    // }
+    // [Theory]
+    // [MemberData(nameof(ExtrudeWallBaseTo3DMeshData))]
+    // public void extrude_wallbase_to_3d_mesh_data(WallBase wallBase, Point3D[] expectedPoints, int[][] expectedFaceIndices)
+    // {
+    //     var actual = WallExtensions.ExtrudeToMeshData(wallBase);
 
-        Point3D[] actualPoints = [.. actual.Points];
-        Assert.Equal(expectedPoints, actualPoints);
+    //     Point3D[] actualPoints = [.. actual.Points];
+    //     Assert.Equal(expectedPoints, actualPoints);
 
-        int[][] actualFaceIndices = [.. actual.FaceIndices];
-        Assert.Equal(expectedFaceIndices, actualFaceIndices);
-    }
+    //     int[][] actualFaceIndices = [.. actual.FaceIndices];
+    //     Assert.Equal(expectedFaceIndices, actualFaceIndices);
+    // }
 }
