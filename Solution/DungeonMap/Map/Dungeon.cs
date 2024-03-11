a4d2d3fcae12d55259f8d17459b2b66d72f0c6fb
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-03-11 05:42:09 -0700

    feat: Dungeon should return tile walls.

---
+++ Solution/DungeonMap/Map/Dungeon.cs
@@ -0,0 +1,19 @@
+namespace CaptainCoder.Dungeoneering.DungeonMap;
+
+public class Dungeon()
+{
+    public WallMap Walls { get; private set; } = new();
+    public Tile GetTile(Position position)
+    {
+        Walls.TryGetWall(position, Facing.North, out WallType north);
+        Walls.TryGetWall(position, Facing.East, out WallType east);
+        Walls.TryGetWall(position, Facing.South, out WallType south);
+        Walls.TryGetWall(position, Facing.West, out WallType west);
+        TileWalls walls = new(north, east, south, west);
+        return new Tile(position, walls);
+    }
+}
+
+public record Tile(Position Position, TileWalls Walls);
+
+public record TileWalls(WallType North, WallType East, WallType South, WallType West);
