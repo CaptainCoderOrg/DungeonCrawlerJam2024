f1de1240922adf70041fb8091718268303481930
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-03-12 15:28:50 -0700

    chore: Remove Lua references.

--- Solution/Unity/DungeonCrawlerUnity/CrawlingModeController.cs
+++ Solution/Unity/DungeonCrawlerUnity/CrawlingModeController.cs
@@ -1,3 +1,4 @@
+using CaptainCoder.Dungeoneering.DungeonMap;
 using CaptainCoder.Dungeoneering.DungeonMap.Unity;
 using CaptainCoder.Dungeoneering.Player;
 using CaptainCoder.Dungeoneering.Player.Unity;
@@ -6,12 +7,23 @@ using UnityEngine;
 
 namespace CaptainCoder.Dungeoneering.Unity;
 
-public class CrawlingModeController : MonoBehaviour
+public class CrawlingModeController : MonoBehaviour, ITileEventContext
 {
     [field: SerializeField]
     public DungeonController DungeonController { get; set; } = default!;
     [field: SerializeField]
     public PlayerViewController PlayerViewController { get; set; } = default!;
+    public PlayerView View
+    {
+        get => PlayerViewController.View;
+        set
+        {
+            PlayerViewController.View = value;
+            Debug.Log($"PlayerView: {value}");
+        }
+    }
+
+    public Dungeon Dungeon { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
     public void HandleMovement(MovementAction action)
     {
