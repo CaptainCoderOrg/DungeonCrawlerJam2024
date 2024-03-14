e68ed60484a919cdb34b9289e97dd236a7aaaf11
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-03-14 06:29:52 -0700

    refactor: Simplify Interpreter calls.

--- Solution/DungeonCrawler.Lua/Interpreter.cs
+++ Solution/DungeonCrawler.Lua/Interpreter.cs
@@ -10,31 +10,25 @@ public static class Interpreter
     private static bool s_initialized = false;
     public static T EvalLua<T>(string script, IScriptContext context) => EvalLua(script, context).ToObject<T>();
     public static void ExecLua(string script, IScriptContext context) => EvalLua(script, context);
-    public static DynValue EvalLua(string script, IScriptContext context)
-    {
-        Initialize();
-        Script lua = new();
-        lua.Globals.Set("context", UserData.Create(new LuaContext(context)));
-        lua.Globals[Facing.North.ToString()] = (int)Facing.North;
-        lua.Globals[Facing.East.ToString()] = (int)Facing.East;
-        lua.Globals[Facing.South.ToString()] = (int)Facing.South;
-        lua.Globals[Facing.West.ToString()] = (int)Facing.West;
-        return lua.DoString(script);
-    }
-    public static T EvalRawLua<T>(string script) => EvalRawLua(script).ToObject<T>();
-    private static DynValue EvalRawLua(string script)
-    {
-        Initialize();
-        Script lua = new();
-        lua.Globals.Set("LuaAPI", UserData.Create(new LuaAPI()));
-        return lua.DoString(script);
-    }
-    private static void Initialize()
+    public static DynValue EvalLua(string script, IScriptContext context) => Initialize(context).DoString(script);
+    public static T EvalRawLua<T>(string script) => Initialize().DoString(script).ToObject<T>();
+    private static Script Initialize(IScriptContext? context = null)
     {
         if (!s_initialized)
         {
             LuaInitializer.Initialize();
             s_initialized = true;
         }
+        Script lua = new();
+        if (context is not null)
+        {
+            lua.Globals.Set("context", UserData.Create(new LuaContext(context)));
+        }
+        lua.Globals.Set("LuaAPI", UserData.Create(new LuaAPI()));
+        lua.Globals[Facing.North.ToString()] = (int)Facing.North;
+        lua.Globals[Facing.East.ToString()] = (int)Facing.East;
+        lua.Globals[Facing.South.ToString()] = (int)Facing.South;
+        lua.Globals[Facing.West.ToString()] = (int)Facing.West;
+        return lua;
     }
 }
