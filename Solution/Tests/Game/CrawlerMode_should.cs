f50aa93ca194d2a1a84c29dd2f2095ae56f2a22c
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-03-15 07:59:05 -0700

    feat: CrawlerMode should notify OnViewChange

---
+++ Solution/Tests/Game/CrawlerMode_should.cs
@@ -0,0 +1,26 @@
+namespace Tests;
+
+using CaptainCoder.Dungeoneering.DungeonMap;
+using CaptainCoder.Dungeoneering.Game;
+using CaptainCoder.Dungeoneering.Player;
+
+using Shouldly;
+
+public class CrawlerMode_should
+{
+    [Theory]
+    [InlineData(5, 5, Facing.East)]
+    [InlineData(7, 2, Facing.West)]
+    [InlineData(1, 9, Facing.North)]
+    [InlineData(3, 18, Facing.South)]
+    public void notify_observer_when_view_changes(int x, int y, Facing facing)
+    {
+        CrawlerMode underTest = new(new Dungeon(), new PlayerView(new Position(0, 0), Facing.North));
+        PlayerView actual = null!;
+        underTest.OnViewChange += (newView) => actual = newView;
+        PlayerView expected = new(x, y, facing);
+        underTest.CurrentView = expected;
+
+        actual.ShouldBe(expected);
+    }
+}
