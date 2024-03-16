cc1e61715e3c285169f057931647a68166d97f27
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-03-16 09:00:48 -0700

    refactor: DungeonEventMap should serialize to Json

--- Solution/DungeonCrawler.IO/DungeonEventMapJsonConverter.cs
+++ Solution/DungeonCrawler.IO/DungeonEventMapJsonConverter.cs
@@ -7,17 +7,14 @@ public class DungeonEventMapJsonConverter : JsonConverter<DungeonEventMap>
     public static DungeonEventMapJsonConverter Shared { get; } = new();
     public override DungeonEventMap? ReadJson(JsonReader reader, Type objectType, DungeonEventMap? existingValue, bool hasExistingValue, JsonSerializer serializer)
     {
-        string data = reader.Value as string ?? throw new Exception($"Could not read DungeonEventMap.");
-        (Position, TileEvent[])[] events = JsonConvert.DeserializeObject<(Position, TileEvent[])[]>(data) ?? throw new Exception($"Could not read DungeonEventMap.");
+        (Position, TileEvent[])[] events = serializer.Deserialize<(Position, TileEvent[])[]>(reader) ?? throw new Exception($"Could not read DungeonEventMap.");
         return new DungeonEventMap(events.Select(pair => (pair.Item1, pair.Item2.AsEnumerable())));
     }
 
     public override void WriteJson(JsonWriter writer, DungeonEventMap? value, JsonSerializer serializer)
     {
-        // throw new NotImplementedException();
         if (value is null) { throw new Exception($"Could not write null DungeonEventMap."); }
         (Position, TileEvent[])[] events = [.. value.Events.Select(kvp => (kvp.Key, kvp.Value.ToArray()))];
-        string json = JsonConvert.SerializeObject(events);
-        writer.WriteValue(json);
+        serializer.Serialize(writer, events);
     }
 }
