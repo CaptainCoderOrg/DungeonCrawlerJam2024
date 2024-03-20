2a629207f43bf22d38d9ce7757b565e6d44dd2de
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-03-20 13:56:53 -0700

    refactor: Update engine for pixel art.

--- Solution/build.sh
+++ Solution/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dotnet DungeonCrawler.Editor/bin/Debug/net8.0/DungeonCrawler.Editor.dll -b "Test Crawler" -o ../DC\ Jam\ 2024\ Unity/Assets/Data/Manifests/Test.json
+dotnet DungeonCrawler.Editor/bin/Debug/net8.0/DungeonCrawler.Editor.dll -b "PixelDungeon" -o ../DC\ Jam\ 2024\ Unity/Assets/Data/Manifests/Test.json
