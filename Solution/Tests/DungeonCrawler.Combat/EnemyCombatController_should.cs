6c0204aa2d1168cc220a266fcd47893f9f3a6198
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-03-27 09:13:54 -0700

    chore: Added failing tests.

--- Solution/Tests/DungeonCrawler.Combat/EnemyCombatController_should.cs
+++ Solution/Tests/DungeonCrawler.Combat/EnemyCombatController_should.cs
@@ -116,21 +116,102 @@ public class EnemyCombatController_should
 
         bool result = actualMoves.SequenceEqual(expectedMoves);
         result.ShouldBeTrue();
-        /*
-        #########
-        #########
-        A########
-        1########
-        */
     }
 
-    public void not_allow_enemies_to_move_through_players()
+    // In this setup, A is a player Character.
+    // 1 is the enemy who has a speed of 3
+    // 2 is an enemy that blocks
+    public static IEnumerable<object[]> AllowEnemyToMoveThroughEnemiesData => [
+        [
+            """
+            #########
+            #########
+            #########
+            #########
+            """,
+            """
+            A########
+            #########
+            2########
+            1########
+            """,
+            // Enemy moves 2 spaces to reach player
+            (Position[])[new Position(0, 2), new Position(0, 1)],
+        ]
+        ,
+        [
+            """
+            #########
+            #########
+            #########
+            #########
+            """,
+            """
+            #########
+            #########
+            #########
+            1#2#A####
+            """,
+            // Enemy moves 3 spaces to reach player
+            (Position[])[new Position(1, 3), new Position(2, 3), new Position(3, 3),],
+        ]
+        ,
+        [
+            """
+            #########
+            #########
+            #########
+            #########
+            """,
+            """
+            #########
+            #########
+            #########
+            1##2####A
+            """,
+            // Enemy moves 3 spaces toward player moving around the other enemy
+            (Position[])[new Position(1, 3), new Position(2, 3), new Position(3, 2),],
+        ]
+        ,
+        [
+            """
+            #########
+            #########
+            #########
+            #########
+            """,
+            """
+            ###A#####
+            ##2######
+            #########
+            1########
+            """,
+            // Enemy moves 2 spaces diagonally
+            (Position[])[new Position(1, 2), new Position(2, 1), new Position(3, 1)],
+        ]
+    ];
+    [Theory]
+    [MemberData(nameof(AllowEnemyToMoveThroughEnemiesData))]
+    public void allow_enemy_to_move_through_enemies(string map, string setup, Position[] expectedMoves)
     {
+        Dictionary<char, HashSet<Position>> setupMap = CombatMapExtensions.ParseCharPositions(setup);
+        CombatMap underTest = new()
+        {
+            Tiles = CombatMapExtensions.ParseTiles(map),
+            PlayerCharacters = new Dictionary<Position, PlayerCharacter>()
+            {
+                { setupMap['A'].First(), new PlayerCharacter() },
+            },
+            Enemies = new Dictionary<Position, Enemy>()
+            {
+                { setupMap['1'].First(), new Enemy() { Card = new EnemyCard() { Speed = 3 }}},
+                { setupMap['2'].First(), new Enemy() }
+            }
+        };
 
-    }
+        IEnumerable<Position> actualMoves = underTest.GetEnemyMove(setupMap['1'].First());
 
-    public void allow_enemies_to_move_through_enemies()
-    {
-        
+        bool result = actualMoves.SequenceEqual(expectedMoves);
+        result.ShouldBeTrue();
     }
 }
